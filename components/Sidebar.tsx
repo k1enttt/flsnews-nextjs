0ffebffc5922f59265baf3e2aabf2209b3bb8a13
Tạ Thúc Trung Kiên <tathuctrungkien2003@gmail.com>
2024-12-15 17:40:55 +0700

    Fix: Sidebar is partially hidden on mobile view

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -92,7 +92,7 @@ function Sidebar({}) {
       className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-blue-dark sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
       aria-label="Sidebar"
     >
-      <div className="h-full px-3 pb-4 overflow-y-auto dark:bg-gray-800">
+      <div className="h-full px-3 overflow-y-auto dark:bg-gray-800 md:pb-4 pb-16">
         <ul className="space-y-2 font-medium">
           <li>
             <AccordianTags label="Origin" childrenTags={tags.origin} />
