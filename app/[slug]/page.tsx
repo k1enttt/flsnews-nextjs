0011dc542319575cf059d8f044973a5d5fed2aa9
Tạ Thúc Trung Kiên <tathuctrungkien2003@gmail.com>
2024-12-16 13:23:27 +0700

    Add author and published date of blog

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -7,6 +7,7 @@ import { Textarea } from "flowbite-react";
 export default function BlogPage() {
   const title: string = "Best practices for successful prototypes";
   const commentCount = 20;
+  const author = "Admin";
   return (
     <BlogLayout>
       {/* Thẻ main là bài blog + phần comment */}
@@ -22,6 +23,14 @@ export default function BlogPage() {
               <h1 className="mb-4 text-3xl font-extrabold leading-tight lg:mb-6 font-conthrax-bold">
                 {title.toUpperCase()}
               </h1>
+              <p>
+                <i>
+                  By <b>{author}</b>
+                </i>{" "}
+                on <time dateTime="2022-03-12" title="March 12th, 2022">
+                  Mar. 12, 2022
+                </time>
+              </p>
             </header>
             {/* Nội dung bài blog */}
             <img
@@ -352,9 +361,9 @@ export default function BlogPage() {
                   viewBox="0 0 24 24"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                   <path d="M7.2 8.809H4V19.5h3.2V8.809Z" />
                 </svg>
@@ -371,9 +380,9 @@ export default function BlogPage() {
                   aria-hidden="true"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
               </a>
@@ -392,9 +401,9 @@ export default function BlogPage() {
                   viewBox="0 0 24 24"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839 4.225 4.225 0 0 0-.79 1.965 30.146 30.146 0 0 0-.2 3.206v1.5a30.12 30.12 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972.604.536 1.38.837 2.187.848 1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.844 2.844 0 0 0 1.985-.84 4.27 4.27 0 0 0 .787-1.965 30.12 30.12 0 0 0 .2-3.206v-1.516a30.672 30.672 0 0 0-.202-3.206Zm-11.692 6.554v-5.62l5.4 2.819-5.4 2.801Z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
               </a>
