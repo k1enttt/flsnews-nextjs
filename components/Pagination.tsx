bb84d1b5bdee3f258f60501c71ee2b59a999cb61
Tạ Thúc Trung Kiên <81433114+kenVis1529@users.noreply.github.com>
2024-12-22 23:27:09 +0700

    Fix FN-22: Pagination is not sync with searchParams 'page' when searching

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -2,12 +2,14 @@
 
 import { CustomFlowbiteTheme, Pagination } from "flowbite-react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function PostPagination({ totalPages }: { totalPages: number }) {
   const searchParams = useSearchParams();
   const router = useRouter();
-  const [currentPage, setCurrentPage] = useState<number>(parseInt(searchParams.get("page") || "1"));
+  const [currentPage, setCurrentPage] = useState<number>(
+    Number.parseInt(searchParams.get("page") || "1")
+  );
 
   const onPageChange = (page: number) => {
     const params = new URLSearchParams(searchParams);
@@ -16,20 +18,27 @@ export function PostPagination({ totalPages }: { totalPages: number }) {
     router.push(`?${params.toString()}`);
   };
 
+  // useEffect này sẽ chạy mỗi khi currentPage thay đổi
+  // Dùng cho mỗi lần thực hiện search hoặc chọn filter
+  useEffect(() => {
+    setCurrentPage(Number.parseInt(searchParams.get("page") || "1"));
+  }, [searchParams]);
+
   const customPaginationTheme: CustomFlowbiteTheme["pagination"] = {
     pages: {
-        previous: {
-            base:"ml-0 border border-white px-3 py-2 leading-tight text-white enabled:hover:underline"
-        },
-        next: {
-            base:"border border-white px-3 py-2 leading-tight text-white enabled:hover:underline"
-        },
-        selector: {
-            base: "w-12 border border-white py-2 leading-tight text-white enabled:hover:underline",
-            active: "bg-white text-green hover:bg-white hover:text-green hover:underline",
-        }
-    }
-  }
+      previous: {
+        base: "ml-0 border border-white px-3 py-2 leading-tight text-white enabled:hover:underline",
+      },
+      next: {
+        base: "border border-white px-3 py-2 leading-tight text-white enabled:hover:underline",
+      },
+      selector: {
+        base: "w-12 border border-white py-2 leading-tight text-white enabled:hover:underline",
+        active:
+          "bg-white text-green hover:bg-white hover:text-green hover:underline",
+      },
+    },
+  };
 
   return (
     <div className="flex overflow-x-auto sm:justify-center font-gotham-book">
